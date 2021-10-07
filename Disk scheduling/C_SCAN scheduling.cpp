6044bbcea77a394abe18da9a9f5acb9b1411b3f9
Hasanul Bari <hasanul.bari.hasan96@gmail.com>
2021-10-08 00:00:33 +0600

    Create C_SCAN scheduling.cpp

---
+++ Disk scheduling/C_SCAN scheduling.cpp
@@ -0,0 +1,103 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    //cout<<"Enter the size of the disk queue : ";
+    cin>>n;
+
+    int dq[n];
+    //cout<<"Enter the request for cylinders : ";
+    for(int i=0; i<n; i++)
+    {
+        cin>>dq[i];
+    }
+
+    int sz;
+    //cout<<"Enter the size of the disk : ";
+    cin>>sz;
+
+    int head;
+    //cout<<"Enter the starting head position : ";
+    cin>>head;
+
+
+    vector<int> left,right;
+
+    for(int i=0; i<n; i++)
+    {
+        if(dq[i]<head)
+        {
+            left.push_back(dq[i]);
+        }
+        else if(dq[i]>head)
+        {
+            right.push_back(dq[i]);
+        }
+    }
+
+
+    sort(left.begin(),left.end());
+    sort(right.begin(),right.end());
+
+    int output[n], j=0, movement=0, curr_head=head;
+
+    for(int x : right)
+    {
+        //cout<<abs(curr_head-x)<<endl;
+        movement=movement+abs(curr_head-x);
+        output[j]=x;
+        j++;
+        curr_head=x;
+    }
+
+
+    movement=movement+abs(curr_head-(sz-1));
+    movement=movement+(sz-1);
+    curr_head=0;
+
+    for(int x : left)
+    {
+        //cout<<abs(curr_head-x)<<endl;
+        movement=movement+abs(curr_head-x);
+        output[j]=x;
+        j++;
+        curr_head=x;
+    }
+
+
+
+
+
+
+
+    cout<<"Total head movement "<<movement<<" cylinders"<<endl;
+    cout<<"Order of service : "<<endl<<head;
+    for(int i=0; i<n; i++)
+    {
+        cout<<" -> "<<output[i];
+    }
+    cout<<endl;
+
+
+
+
+    return 0;
+}
+
+/*
+8
+98 183 37 122 14 124 65 67
+200
+53
+*/
+
+/*
+8
+176 79 34 60 92 11 41 114
+200
+50
+*/
+
+
